fcfc7bb7afbcc71f56f4bfe47f495b7fb1154348
Sparsematrix09 <bt23cse041@iiitn.ac.in>
2025-12-29 22:13:54 +0530

    mistake fix

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -1,37 +1,48 @@
-import { Inngest } from 'inngest';
-import { connectDB } from './db.js';
-import User from '../models/User.js';
+import { Inngest } from "inngest";
+import { connectDB } from "./db.js";
+import User from "../models/User.js";
 
 export const inngest = new Inngest({ id: "Interveasy" });
 
+const syncUser = inngest.createFunction(
+  { id: "sync-user" },
+  { event: "clerk/user.created" },
+  async ({ event }) => {
+    await connectDB();
 
-const syncUser=inngest.createFunction(
-    {id:"sync-user"},
-    {event:"clerk/user.created"},
-    async({event})=>{
-        await connectDB()
-        const{id,email_addresses,first_name,last_name,profile_image_url}=event.data
-        const newUser={
-            clerkid:id,
-            email:email_addresses[0]?.email_address,
-            name:`${first_name || ""} ${last_name || ""}`,
-            profileImage:profile_image_url || ""
-        }
-        await User.create(newUser);
+    const {
+      id,
+      email_addresses,
+      first_name,
+      last_name,
+      profile_image_url
+    } = event.data;
 
-        //todo something
-    }
+    const newUser = {
+      clerkId: id, // ✅ MUST MATCH SCHEMA
+      email: email_addresses[0]?.email_address,
+      name: `${first_name || ""} ${last_name || ""}`,
+      profileImage: profile_image_url || ""
+    };
+
+    await User.create(newUser);
+
+    return { success: true };
+  }
 );
 
-const deleteUserFromDB=inngest.createFunction(
-    {id:"delete-user-from-db"},
-    {event:"clerk/user.deleted"},
-    async({event})=>{
-        await connectDB()
-        await User.deleteOne({clerkid:id});
+const deleteUserFromDB = inngest.createFunction(
+  { id: "delete-user-from-db" },
+  { event: "clerk/user.deleted" },
+  async ({ event }) => {
+    await connectDB();
 
-        //doing something else
-    }
+    const { id } = event.data;
+
+    await User.deleteOne({ clerkId: id });
+
+    return { success: true };
+  }
 );
 
-export const functions = [syncUser,deleteUserFromDB];
+export const functions = [syncUser, deleteUserFromDB];
