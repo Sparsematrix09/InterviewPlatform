e2bed7a341b9f6421f61552d9afb0c8a5cd91747
Sparsematrix09 <bt23cse041@iiitn.ac.in>
2025-12-29 21:15:03 +0530

    sync clerk and inngest and also db

---
+++ backend/src/lib/inngest.js
@@ -0,0 +1,37 @@
+import { Inngest } from 'inngest';
+import { connectDB } from './db.js';
+import User from '../models/User.js';
+
+export const inngest = new Inngest({ id: "Interveasy" });
+
+
+const syncUser=inngest.createFunction(
+    {id:"sync-user"},
+    {event:"clerk/user.created"},
+    async({event})=>{
+        await connectDB()
+        const{id,email_addresses,first_name,last_name,profile_image_url}=event.data
+        const newUser={
+            clerkid:id,
+            email:email_addresses[0]?.email_address,
+            name:`${first_name || ""} ${last_name || ""}`,
+            profileImage:profile_image_url || ""
+        }
+        await User.create(newUser);
+
+        //todo something
+    }
+);
+
+const deleteUserFromDB=inngest.createFunction(
+    {id:"delete-user-from-db"},
+    {event:"clerk/user.deleted"},
+    async({event})=>{
+        await connectDB()
+        await User.deleteOne({clerkid:id});
+
+        //doing something else
+    }
+);
+
+export const functions = [syncUser,deleteUserFromDB];
--- backend/src/server.js
+++ backend/src/server.js
@@ -2,10 +2,17 @@ import express from 'express';
 import path from 'path';
 import {ENV} from './lib/env.js';
 import { connectDB } from "./lib/db.js";
-
+import cors from 'cors';
+import { serve } from "inngest/express";
+import { inngest, functions } from './lib/inngest.js';
 const app = express();
 
 const __dirname = path.resolve();
+//middleware
+app.use(express.json());
+//credentils true to allow cookies from frontend
+app.use(cors({origin:ENV.CLIENT_URL,credentials:true}));
+app.use("/api/inngest",serve({client:inngest,functions}))
 
 app.get('/health', (req, res) => {
   res.status(200).json({msg:'api is up and running'});
