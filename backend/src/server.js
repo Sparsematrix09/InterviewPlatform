0fb91d59512efb547b84fcc065722db8eb2a3bc8
Sparsematrix09 <bt23cse041@iiitn.ac.in>
2025-12-29 21:50:54 +0530

    final done

--- backend/src/server.js
+++ backend/src/server.js
@@ -17,32 +17,25 @@ app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
 app.use("/api/inngest", serve({ client: inngest, functions }));
 
 // API routes
-app.get('/health', (req, res) => {
-  res.status(200).json({ msg: 'api is up and running' });
-});
-app.get('/books', (req, res) => {
-  res.status(200).json({ msg: 'this is book endpoint' });
+app.get("/health", (req, res) => {
+  res.status(200).json({ msg: "api is up and running" });
 });
 
-// Serve frontend in production
-if (ENV.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+// make our app ready for deployment
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
 
-// Start server
 const startServer = async () => {
   try {
-    await connectDB();  
-    app.listen(ENV.PORT, () => {
-      console.log(`Server running on port ${ENV.PORT}`);
-    });
+    await connectDB();
+    app.listen(ENV.PORT, () => console.log("Server is running on port:", ENV.PORT));
   } catch (error) {
-    console.error("❌ Failed to start server", error);
-    process.exit(1);
+    console.error("💥 Error starting the server", error);
   }
 };
 
