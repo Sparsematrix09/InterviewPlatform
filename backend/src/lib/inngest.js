94c29c8422352396c78cbb1df18e5defd9b7231e
Sparsematrix09 <bt23cse041@iiitn.ac.in>
2025-12-29 22:56:11 +0530

    syncing stream

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -1,6 +1,7 @@
 import { Inngest } from "inngest";
 import { connectDB } from "./db.js";
 import User from "../models/User.js";
+import { upsertStreamUser } from "./stream.js";
 
 export const inngest = new Inngest({ id: "Interveasy" });
 
@@ -27,7 +28,11 @@ const syncUser = inngest.createFunction(
 
     await User.create(newUser);
 
-    return { success: true };
+    await upsertStreamUser({
+        id:newUser.clerkId.toString(),
+        name:newUser.name,
+        image:newUser.profileImage
+    })
   }
 );
 
@@ -41,7 +46,7 @@ const deleteUserFromDB = inngest.createFunction(
 
     await User.deleteOne({ clerkId: id });
 
-    return { success: true };
+    await deleteStreamUser(id.toString());
   }
 );
 
---
+++ backend/src/lib/stream.js
@@ -0,0 +1,29 @@
+import {StreamChat} from "stream-chat"
+import {ENV} from "./env.js"
+import { use } from "react"
+
+const apiKey= ENV.STREAM_API_KEY
+const apiSecret= ENV.STREAM_API_SECRET
+
+if(!apiKey || !apiSecret){
+    console.error("❌ Stream API key or secret is not defined in environment variables")
+}
+export const chatClient= StreamChat.getInstance(apiKey, apiSecret)
+
+export const upsertStreamUser= async(userData)=>{
+    try{
+        await chatClient.upsertUser(userData)
+        return userData
+    }catch(error){  
+        console.error("❌ Error upserting Stream user", error)
+    }
+}
+
+export const deleteStreamUser= async(userId)=>{
+    try{
+        await chatClient.deleteUser(userId)
+        return { success: true }
+    }catch(error){  
+        console.error("❌ Error deleting Stream user", error)
+    }
+}
