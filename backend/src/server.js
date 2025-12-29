f295191f317dd8a47aa7e27d2364e863ab44c5a1
Sparsematrix09 <bt23cse041@iiitn.ac.in>
2025-12-29 21:36:43 +0530

    done function change for frontend production

--- backend/src/server.js
+++ backend/src/server.js
@@ -26,13 +26,10 @@ app.get('/books', (req, res) => {
 
 // Serve frontend in production
 if (ENV.NODE_ENV === 'production') {
-  const frontendPath = path.join(__dirname, '../frontend/dist');
-  console.log('Serving frontend from:', frontendPath);
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
-  app.use(express.static(frontendPath));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(frontendPath, 'index.html'));
+  app.get('/{*any}', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
   });
 }
 
